2ba1d860c0683c8331f393715c2fa22b0dabd992
saturnman <saturnman@126.com>
2018-06-15 02:31:26 +0800

    update script for first remote old containers and images

--- deploy_easycloudapp.sh
+++ deploy_easycloudapp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 apt-get update
 apt-get install -y docker.io
+docker ps  |grep 'saturnman/easycloudapp' |awk '{print $1}'|xargs docker kill
+docker ps -a  |grep 'saturnman/easycloudapp' |awk '{print $1}'|xargs docker rm
+docker rmi saturnman/easycloudapp
 docker pull saturnman/easycloudapp
 docker run -d -p 10080:80 -v /var/run/docker.sock:/var/run/docker.sock saturnman/easycloudapp
